ff28cf473e6be7f64aba82f939366b90d1f3edcf
marcospha@gmail.com <marcospha@gmail.com@f35dcd19-5975-300e-6108-adf6f51e53a5>
2015-01-22 15:05:12 +0000

    Aplicación inicial en Yii 2 (plantilla avanzada) con las vistas de Balance General y Estado de Resultados creadas.

---
+++ frontend/config/main-local.php
@@ -0,0 +1,21 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'kexDwzggZQHGeQ0h0u2UasiA6P5negMn',
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = 'yii\debug\Module';
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = 'yii\gii\Module';
+}
+
+return $config;
