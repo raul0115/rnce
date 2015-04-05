057f9648725c9244dd85782b2120543e5a60a1c8
Edgar Leal <edgar.leal0@gmail.com>
2015-04-04 20:36:07 -0430

    archivos restablecidos
    archivos restablecidos

---
+++ frontend/views/site/_resultados.php
@@ -0,0 +1,246 @@
+<table data-toggle="table" data-url="data1.json" data-cache="false" data-height="">
+		    <thead>
+		        <tr class="principaltr">
+		            <th data-field=""> </th>
+		            <th data-field="">NOTA</th>
+		            <th data-field="">ANEXO</th>
+		            <th data-field="">20X2</th>
+		            <th data-field="">20X1</th>
+		        </tr>
+		    </thead>
+
+		
+			<!-- INGRESOS OPERACIONALES -->
+		    <tbody>
+
+				<tr>
+	        		<td colspan="2" class="divider"><b>INGRESOS OPERACIONALES</b> <hr /></td>
+	    		</tr>
+	    		
+			    	<tr class="principaltr">
+
+			    		<td>Provenientes del Sector Público</td>
+			    		<td>XX</td>
+			    		<td>I-2</td>
+			    		<td>0,00</td>
+			    		<td>0,00</td>
+			    	</tr>
+			    	<tr class="principaltr">
+
+			    		<td>Provenientes del Sector Privado</td>
+			    		<td>XX</td>
+			    		<td>I-1</td>
+			    		<td>0,00</td>
+			    		<td>0,00</td>
+			    	</tr>
+
+			    <tr>
+	        		<td colspan="2" class="divider"> <hr /><b><i>TOTAL INGRESOS OPERACIONALES</i></b> <hr /> </td>
+			    	<td></td>
+			    	<td>0,00</td>
+			    	<td>0,00</td>
+	    		</tr>
+
+		    </tbody>
+
+			<!-- COSTOS OPERACIONALES -->
+			<tbody>
+
+				<tr>
+	        		<td colspan="2" class="divider"><b>COSTOS OPERACIONALES</b> <hr /></td>
+	    		</tr>
+	    		
+			    	<tr class="principaltr">
+
+			    		<td>Costo de producción y ventas</td>
+			    		<td>XX</td>
+			    		<td>I-3</td>
+			    		<td>101.604,61</td>
+			    		<td>119.929,32</td>
+			    	</tr>
+			    	<tr class="principaltr">
+
+			    		<td>Costo de ventas</td>
+			    		<td>XX</td>
+			    		<td>I-3</td>
+			    		<td>0,00</td>
+			    		<td>0,00</td>
+			    	</tr>
+			    	<tr class="principaltr">
+
+			    		<td>Costo de construcción o ejecución de obras</td>
+			    		<td>XX</td>
+			    		<td>I-3</td>
+			    		<td>0,00</td>
+			    		<td>0,00</td>
+			    	</tr>
+
+			    	<tr class="principaltr">
+
+			    		<td>Otros Costos</td>
+			    		<td>XX</td>
+			    		<td>I-3</td>
+			    		<td>0,00</td>
+			    		<td>0,00</td>
+			    	</tr>
+
+			    <tr>
+	        		<td colspan="2" class="divider"> <hr /><b><i>TOTAL COSTOS OPERACIONALES</i></b> <hr /> </td>
+					<td></td>
+			    	<td>0,00</td>
+			    	<td>0,00</td>
+	    		</tr>
+
+		    </tbody>
+			<!-- GASTOS OPERACIONALES -->
+
+
+		    <tbody>
+		    	<tr>
+		    		<td colspan="2" class="divider"> <b> RESULTADO BRUTO EN OPERACIONES </b> <hr /><hr /></td>
+		    		<td> </td>
+		    		<td>0,00 <hr /><hr /></td>
+		    		<td>0,00 <hr /><hr /></td>
+		    	</tr>
+		    </tbody>
+
+			<tbody>
+				<tr>
+		    		<td colspan="2" class="divider"> <b> TOTAL OTROS INGRESOS OPERACIONALES </b> <hr /></td>
+		    		<td>XX</td>
+		    		<td>I-6</td>
+		    		<td>0,00 <hr /><hr /></td>
+		    		<td>0,00 <hr /><hr /></td>
+		    	</tr>
+
+		    	<tr>
+		    		<td colspan="2" class="divider"> <b> TOTAL INGRESOS EN OPERACIONES </b> <hr /></td>
+		    		<td> </td>
+		    		<td>0,00 <hr /><hr /></td>
+		    		<td>0,00 <hr /><hr /></td>
+		    	</tr>
+
+		    </tbody>
+			
+			<!-- GASTOS OPERACIONALES -->
+			<tbody>
+
+				<tr>
+	        		<td colspan="2" class="divider"><b>GASTOS OPERACIONALES</b> <hr /></td>
+	    		</tr>
+	    		
+			    	<tr class="principaltr">
+
+			    		<td>Gastos de distribución y venta</td>
+			    		<td>XX</td>
+			    		<td>II-1</td>
+			    		<td>0,00</td>
+			    		<td>0,00</td>
+			    	</tr>
+			    	<tr class="principaltr">
+
+			    		<td>Gastos de administración</td>
+			    		<td>XX</td>
+			    		<td>II-1</td>
+			    		<td>0,00</td>
+			    		<td>0,00</td>
+			    	</tr>
+
+			    <tr>
+	        		<td colspan="2" class="divider"> <hr /><b><i>TOTAL GASTOS OPERACIONALES</i></b> <hr /> </td>
+					<td></td>
+			    	<td>0,00</td>
+			    	<td>0,00</td>
+	    		</tr>
+
+		    </tbody>
+
+
+		    <tbody>
+		    	<tr>
+		    		<td colspan="2" class="divider"> <b> RESULTADO EN OPERACIONES </b> </td>
+		    		<td> </td>
+		    		<td>0,00 </td>
+		    		<td>0,00 </td>
+		    	</tr>
+		    </tbody>
+
+
+			<tbody>
+				<tr>
+		    		<td colspan="2" class="divider"> <b> TOTAL OTRAS GANANCIAS O (PÉRDIDAS)</b> <hr /></td>
+		    		<td>XX</td>
+		    		<td>III</td>
+		    		<td>0,00 </td>
+		    		<td>0,00 </td>
+		    	</tr>
+
+		    	<tr>
+		    		<td colspan="2" class="divider"> <b> TOTAL BENEFICIO O (COSTO) DE FINANCIAMIENTO </b> <hr /></td>
+		    		<td>XX</td>
+		    		<td>IV</td>
+		    		<td>0,00 </td>
+		    		<td>1,00 </td>
+		    	</tr>
+
+		    </tbody>
+
+		    <tbody>
+		    	<tr>
+		    		<td colspan="2" class="divider"> <b> RESULTADO ANTES DE IMPUESTOS</b> </td>
+		    		<td> </td>
+		    		<td>0,00 </td>
+		    		<td>0,00 </td>
+		    	</tr>
+		    	<tr>
+		    		<td colspan="2" class="divider"> <b> TOTAL INGRESO O (GASTO) IMPUESTO CORRIENTE</b> </td>
+		    		<td>XX</td>
+		    		<td>DD-1</td>
+		    		<td>0,00 </td>
+		    		<td>34.071,28 </td>
+		    	</tr>
+		    	<tr>
+		    		<td colspan="2" class="divider"> <b> TOTAL INGRESO O (GASTO) IMPUESTO DIFERIDO</b> </td>
+		    		<td>XX</td>
+		    		<td>DD-2</td>
+		    		<td>53,60 </td>
+		    		<td>101.537,51 </td>
+		    	</tr>
+		    </tbody>
+
+
+		    <tbody>
+		    	<tr>
+		    		<td colspan="2" class="divider"> <b> RESULTADO DEL PERÍODO </b> </td>
+		    		<td> </td>
+		    		<td>0,00 </td>
+		    		<td>0,00 </td>
+		    	</tr>
+		    </tbody>
+
+		    <tbody>
+		    	<tr>
+		    		<td colspan="2" class="divider"> <b> RESULTADO DEL PERÍODO</b> </td>
+		    		<td> </td>
+		    		<td>0,00 </td>
+		    		<td>0,00 </td>
+		    	</tr>
+		    	<tr>
+		    		<td colspan="2" class="divider"> <b> TOTAL FONDOS Y RESERVAS</b> </td>
+		    		<td>XX</td>
+		    		<td>MM-2</td>
+		    		<td>0,00 </td>
+		    		<td>0,00 </td>
+		    	</tr>
+		    	<tr>
+		    		<td colspan="2" class="divider"> <b> TOTAL OTRO RESULTADO INTEGRAL</b> </td>
+		    		<td>XX</td>
+		    		<td>MM-3</td>
+		    		<td>0,00 </td>
+		    		<td>0,00 </td>
+		    	</tr>
+		    </tbody>
+
+		</table>
+
+		
